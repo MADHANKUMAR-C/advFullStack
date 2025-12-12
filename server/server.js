ac8e32354e3992ed5458e7e8801fa604b122505f
MADHANKUMAR-C <madhankumar8825487841@gmail.com>
2025-12-12 11:58:35 +0530

    Origin changes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 connectDB();
 
 const app= express();
-app.use(cors());
+app.use(cors({"origin":"*"}));
 app.use(express.json());
 app.use('/api', authRoutes);
 app.use('/api', studyPlanRoutes);
